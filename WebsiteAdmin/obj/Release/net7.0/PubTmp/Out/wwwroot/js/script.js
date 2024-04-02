e9f29cd445a3558fd3541d27bc3769bbbeb48cfd
DuyNhan <pnduynhan@gmail.com>
2024-04-02 18:22:47 +0700

    finish admin

---
+++ WebsiteAdmin/obj/Release/net7.0/PubTmp/Out/wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿<script>
+    $(document).ready(function () {
+        $('.nav-link').on('click', function () {
+            // Remove 'active' class from all navigation items
+            $('.nav-link').removeClass('active');
+
+            // Add 'active' class to the clicked navigation item
+            $(this).addClass('active');
+        });
+});
+</script>
